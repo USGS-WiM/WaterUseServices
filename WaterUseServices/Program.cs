c38b5d7242f82f613db13db907723701f6e9dfb7
jknewson <jknewson@usgs.gov>
2017-08-25 15:24:49 -0600

    started dotnet 2 migration

--- WaterUseServices/Program.cs
+++ WaterUseServices/Program.cs
@@ -8,15 +8,13 @@ namespace WaterUseServices
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
+
 }
